e277457c8103b24877e778227d09280462b37365
whdjh <whdjh98@naver.com>
2024-10-01 22:17:53 +0900

    ADD 총점 & 주사위 히스토리

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ function random(n) {
 
 function App() {
 	const [num, setNum] = useState(1);
+	const [sum, setSum] = useState(0);
+	const [gameHistory, setGameHistory] = useState([]);
+	
 	const handleRollClick = () => {
 		const nextNum = random(6);
 		setNum(nextNum);
+		setSum(sum + nextNum);
+		setGameHistory([...gameHistory, nextNum]);
 	};
 	// 1로 초기화
 	const handleClearClick = () => {
@@ -23,7 +28,14 @@ function App() {
 				<Button onClick={handleRollClick}>던지기</Button>
 				<Button onClick={handleClearClick}>처음부터</Button>
 			</div>
-			<Dice color="red" num={num} />
+			<div>
+				<h2>나</h2>
+				<Dice color="blue" num={num} />
+				<h2>총점</h2>
+				<p>{sum}</p>
+				<h2>기록</h2>
+				{gameHistory.join(', ')}
+			</div>
 		</div>
 	);
 }
