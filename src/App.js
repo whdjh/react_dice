4f0117fd4f351fb5e872f1820072e32134e7b44c
whdjh <whdjh98@naver.com>
2024-10-01 20:59:15 +0900

    Implement 주사위 굴리기 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import Button from './Button.js';
 import Dice from './Dice.js';
 
+function random(n) {
+	return Math.ceil(Math.random() * n);
+}
+
 function App() {
+	const [num, setNum] = useState(1);
+	const handleRollClick = () => {
+		const nextNum = random(6);
+		setNum(nextNum);
+	};
+	// 1로 초기화
+	const handleClearClick = () => {
+		setNum(1);
+	};
+
 	return (
 		<div>
-			<Dice color="red" num={2} />
+			<div>
+				<Button onClick={handleRollClick}>던지기</Button>
+				<Button onClick={handleClearClick}>처음부터</Button>
+			</div>
+			<Dice color="red" num={num} />
 		</div>
 	);
 }
