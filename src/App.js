3a5203dde89773f61047dba7463617056e819b97
whdjh <whdjh98@naver.com>
2024-10-02 13:58:22 +0900

    Delete 중복사용 많은 변수 삭제후 계산 추가

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import { useState } from 'react';
-import Button from './Button.js';
-import Dice from './Dice.js';
+import Board from './Board';
+import Button from './Button';
 
 function random(n) {
-	return Math.ceil(Math.random() * n);
+  return Math.ceil(Math.random() * n);
 }
 
 function App() {
-	const [num, setNum] = useState(1);
-	const [sum, setSum] = useState(0);
-	const [gameHistory, setGameHistory] = useState([]);
-	
+	const [myHistory, setMyHistory] = useState([]);
+	const [otherHistory, setOtherHistory] = useState([]);
+
 	const handleRollClick = () => {
 		const nextNum = random(6);
-		setNum(nextNum);
-		setSum(sum + nextNum);
-		setGameHistory([...gameHistory, nextNum]);
+		const nextOtherNum = random(6);
+		setMyHistory([...myHistory, nextNum]);
+		setOtherHistory([...otherHistory, nextOtherNum]);
 	};
-	// 1로 초기화
+
 	const handleClearClick = () => {
-		setNum(1);
+		setMyHistory([]);
+		setOtherHistory([]);
 	};
 
 	return (
 		<div>
-			<div>
-				<Button onClick={handleRollClick}>던지기</Button>
-				<Button onClick={handleClearClick}>처음부터</Button>
-			</div>
-			<div>
-				<h2>나</h2>
-				<Dice color="blue" num={num} />
-				<h2>총점</h2>
-				<p>{sum}</p>
-				<h2>기록</h2>
-				{gameHistory.join(', ')}
-			</div>
+		<Button onClick={handleRollClick}>던지기</Button>
+		<Button onClick={handleClearClick}>처음부터</Button>
+		<div>
+			<Board name="나" color="blue" gameHistory={myHistory} />
+			<Board name="상대" color="red" gameHistory={otherHistory} />
+		</div>
 		</div>
 	);
 }
-
 
 export default App;
